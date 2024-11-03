daf5ef1f05f679214d72d0ede57ebbee60f6eabd
piyushpacharia <ppachariya007@gmail.com>
2024-11-03 14:09:21 +0530

    initial commit

--- src/Server.jsx
+++ src/Server.jsx
@@ -1,2 +1,2 @@
-// export const BASE_URL = "api.reehaai.com"
-export const BASE_URL = "http://localhost:8002"
+export const BASE_URL = "https://api.reehaai.com"
+// export const BASE_URL = "http://localhost:8002"
