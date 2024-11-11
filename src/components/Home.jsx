3c086fb7cd2fb24da89bae7369b638dd243fbac0
piyushpacharia <ppachariya007@gmail.com>
2024-11-11 17:21:05 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         <>
             <Banner />
             <Feature />
-            {/* <About /> */}
+            <About />
             <Mission />
             <Journey />
         </>
--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Images (Replace with actual paths)
 import phase1Image from '../assets/reeha_alldevice.png';
-import phase2Image from '../assets/reeha_tracking.png';
+import phase2Image from '../assets/reeha_track.png';
 import phase3Image from '../assets/ai_tool.png';
 
 export default function Journey() {
@@ -15,7 +15,7 @@ export default function Journey() {
         <div className="flex flex-col md:flex-row items-center" data-aos="fade-right" data-aos-duration="1200" data-aos-once>
           <img src={phase1Image} alt="App Launch" className="w-1/3 h-auto mb-4 md:mb-0 md:mr-6" />
           <div className="text-center md:text-left">
-            <h3 className="text-2xl font-semibold text-gray-700">Phase 1: App Launch</h3>
+            <h3 className="text-3xl font-bold text-black">Phase 1: App Launch</h3>
             <p className="text-gray-600 mt-2">
               Our app aims to empower individuals with real-time assistance through AI. Accessible, easy-to-use, and available to everyone in need.
             </p>
@@ -25,7 +25,7 @@ export default function Journey() {
         {/* Phase 2 - Kids Safety Device */}
         <div className="flex flex-col md:flex-row items-center" data-aos="fade-left" data-aos-duration="1200" data-aos-once>
           <div className="order-2 md:order-1 text-center md:text-left md:mr-6">
-            <h3 className="text-2xl font-semibold text-gray-700">Phase 2: Kids Safety Device</h3>
+            <h3 className="text-3xl font-bold text-black">Phase 2: Kids Safety Device</h3>
             <p className="text-gray-600 mt-2">
               A wearable IoT device for kids, designed to monitor movement and surroundings, providing parents with peace of mind and security.
             </p>
@@ -37,7 +37,7 @@ export default function Journey() {
         <div className="flex flex-col md:flex-row items-center" data-aos="fade-right" data-aos-duration="1200" data-aos-once>
           <img src={phase3Image} alt="Future Innovations" className="w-1/3 h-auto mb-4 md:mb-0 md:mr-6" />
           <div className="text-center md:text-left">
-            <h3 className="text-2xl font-semibold text-gray-700">Phase 3: Future Innovations</h3>
+            <h3 className="text-3xl font-bold text-black">Phase 3: Future Innovations</h3>
             <p className="text-gray-600 mt-2">
               We’re exploring advanced AI and IoT solutions for a broader scope of safety applications. Stay tuned for exciting developments!
             </p>
